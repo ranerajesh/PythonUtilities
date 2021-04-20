2def5f3265720efc99650632aeb804f1b48bf93f
Rajesh Rane <rane.rajeshgec@gmail.com>
2021-04-20 13:21:33 +0530

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,7 +1,13 @@
 def parse_headers(header_line):
+    """
+    Takes a line as input and returns a list of column headers.
+    """
     return header_line.strip().split(',')
 
 def parse_values(data_line):
+    """
+    Takes a line containing data from CSV and returns a list of floating-point numbers.
+    """
     values = []
     for item in data_line.strip().split(','):
         if item == '':
@@ -11,12 +17,25 @@ def parse_values(data_line):
     return values
 
 def create_item_dict(values, headers):
+    """
+    Takes a list of values and a list of headers as inputs and 
+    returns a dictionary with the values associated with their respective headers as keys.
+    
+    Arguments:
+        values - Data line from CSV file
+        headers - Data line from CSV file with headers, i.e. 0th line in CSV
+    """
     result = {}
     for value, header in zip(values, headers):
         result[header] = value
     return result
 
 def read_csv(path):
+    """Reads any CSV file.
+    
+    Arguments:
+        path - CSV file path
+    """
     result = []
     # Open the file in read mode
     with open(path, 'r') as f:
